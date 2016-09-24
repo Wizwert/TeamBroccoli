84dcdac1473796987a1f835bd393470bb7aeaffc
Will <william.rusinko@gmail.com>
2016-09-24 14:22:23 -0500

    Adding the Rest of the models

--- broccoli-api/app/models.py
+++ broccoli-api/app/models.py
@@ -19,3 +19,30 @@ class Meal(Model):
 	meal_location_id = Column(Integer)
 	meal_setting_id = Column(Integer)
 	rating = Column(Double)
+	
+class MealSetting(Model)
+	id = Column(Integer, primary_key=True)
+	description = Column(String(400)
+	
+class MealPart(Model)
+	id = Column(Integer, primary_key=True)
+	meal_id = Column(Integer)
+	part_id = Column(part_id)
+	
+class Student(Model)
+	id = Column(Integer, primary_key=True)
+	first_name = Column(String(400))
+	last_name = Column(String(400))
+	class_id = Column(Integer)
+	
+class MealType(Model)
+	id = Column(Integer, primary_key=True)
+	description = Column(String(400))
+	
+class MealLocation(Model)
+	id = Column(Integer, primary_key=True)
+	description = Column(String(400))
+	
+class MealPart(Model)
+	id = Column(Integer, primary_key=True)
+	description = Column(String(400))
