3ba1455b021c53ab971ee6e0d410debde990ec9b
MJ Berends <mjr.berends@gmail.com>
2016-09-24 14:55:38 -0500

    Update models to contain image logic

--- broccoli-api/app/models.py
+++ broccoli-api/app/models.py
@@ -1,7 +1,12 @@
+
+from flask import url_for
 from flask.ext.appbuilder import Model
 from flask.ext.appbuilder.models.mixins import AuditMixin, FileColumn, ImageColumn
+from flask_appbuilder.models.mixins import ImageColumn
 from sqlalchemy import Column, Integer, String, ForeignKey, Text
 from sqlalchemy.orm import relationship
+# from flask_appbuilder.models import Markup
+
 """
 
 You can use the extra Flask-AppBuilder fields and Mixin's
@@ -33,6 +38,27 @@ class Student(Model):
     last_name = Column(String(255))
     classroom_id = Column(Integer, ForeignKey('classroom.id'))
     classroom = relationship("Classroom")
+    # photo = Column(ImageColumn(size=(300, 300, True), thumbnail_size=(30, 30, True)))
+
+    # def photo_img(self):
+    #     im = ImageManager()
+    #     if self.photo:
+    #         return Markup('<a href="' + url_for('StudentModelView.show',pk=str(self.id)) +\
+    #          '" class="thumbnail"><img src="' + im.get_url(self.photo) +\
+    #           '" alt="Photo" class="img-rounded img-responsive"></a>')
+    #     else:
+    #         return Markup('<a href="' + url_for('StudentModelView.show',pk=str(self.id)) +\
+    #          '" class="thumbnail"><img src="//:0" alt="Photo" class="img-responsive"></a>')
+
+    # def photo_img_thumbnail(self):
+    #     im = ImageManager()
+    #     if self.photo:
+    #         return Markup('<a href="' + url_for('StudentModelView.show',pk=str(self.id)) +\
+    #          '" class="thumbnail"><img src="' + im.get_url_thumbnail(self.photo) +\
+    #           '" alt="Photo" class="img-rounded img-responsive"></a>')
+    #     else:
+    #         return Markup('<a href="' + url_for('StudentModelView.show',pk=str(self.id)) +\
+    #          '" class="thumbnail"><img src="//:0" alt="Photo" class="img-responsive"></a>')
 
     def __repr__(self):
         return "{} {}".format(self.first_name, self.last_name)
